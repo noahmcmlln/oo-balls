e78bb7c1c47351fe05e7cd1259aa2cca9e2cfd0c
noahmcmlln <noahmcmlln@gmail.com>
2016-02-04 21:47:54 -0500

    Typos

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ ballpit.push(new Ball(100, 200));
 ballpit.push(new Ball(75, 250));
 ballpit.push(new Ball(200, 200));
 
-ballpit[0].speed.x = 2
-ballpit[0].speed.y = 3
+ballpit[0].speed.x = 2;
+ballpit[0].speed.y = 3;
 
-ballpit[1].speed.x = 4
+ballpit[1].speed.x = 4;
 
 setup = function () {
   createCanvas(600, 400);
