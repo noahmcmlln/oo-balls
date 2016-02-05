5d13773c4b4e32af861a2249440cf24bb883d443
noahmcmlln <noahmcmlln@gmail.com>
2016-02-04 21:38:11 -0500

    Edit oo-balls  - in-class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
-var myBall = new Ball(100, 100);
-myBall.speed.x = 2;
-myBall.speed.y = 2;
+var ballpit = [];
+
+ballpit.push(new Ball(100, 200));
+ballpit.push(new Ball(75, 250));
+ballpit.push(new Ball(200, 200));
+
+ballpit[0].speed.x = 2
+ballpit[0].speed.y = 3
+
+ballpit[1].speed.x = 4
 
 setup = function () {
   createCanvas(600, 400);
@@ -8,6 +15,8 @@ setup = function () {
 
 draw = function () {
   background(100);
-  myBall.update();
-  myBall.display();
+  ballpit.forEach(function updateAndDisplay (ball) {
+    ball.update();
+    ball.display();
+  });
 };
